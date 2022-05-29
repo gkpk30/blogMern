c2f846262ee7bd4266b96c0a9581ce81df602f24
gkpk30 <guykoomer@hotmail.com>
2022-05-29 14:30:06 -0700

    trying to deploy

--- api/index.js
+++ api/index.js
@@ -10,10 +10,13 @@ const multer = require("multer");
 const path = require("path");
 const cors = require('cors')
 
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(cors())
 app.use(express.json());
+
+
 app.use("/images", express.static(path.join(__dirname, "/images"))); 
 
 mongoose
@@ -25,6 +28,8 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+  
+  
 
   //creates storage for images
 const storage = multer.diskStorage({
@@ -46,6 +51,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
-  console.log("Backend is running. 5000");
+app.listen(PORT, () => {
+  console.log(`Backend is running. ${PORT}`);
 });
